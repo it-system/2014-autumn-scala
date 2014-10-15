1ab812e9e369573d3ef52ace20a9ff1724c30046
大田祐輔 <yusuke@net84-dhcp-129.sfc.keio.ac.jp>
2014-10-15 18:05:25 +0900

    10月16日発表用の修正とsrcの追加

---
+++ src/141016_src/PrimitiveObject.scala
@@ -0,0 +1,23 @@
+/**
+ * it-system
+ * 2014/10/16 4限
+ * Scalaは、すべてオブジェクト型でできている？
+ * ⇛YES 
+ * Javaでは、プリミティブ型として扱っていたInt、FloatなどもScalaでは
+ * すべてオブジェクト型として扱う
+ */
+
+package scala141016
+
+object PrimitiveObject {
+
+  def main(args: Array[String]): Unit = {
+    var value1:Int = 1;
+    value1.toString  //オブジェクトなのでメソッドを呼び出せる Int⇛String
+    
+    val value2:Byte = 10;
+    value2.toChar  //Byte ⇛ Char
+    
+  }
+
+} 
---
+++ src/141016_src/Trait.scala
@@ -0,0 +1,48 @@
+/**
+ * it-system
+ * 2014/10/16 4限
+ * taritってなに？
+ * ⇛Javaのインターフェースのようなもの
+ * ⇛メソッド定義と実装したメソッドも扱える
+ * ⇛多重継承が可能
+ */
+package scala141016
+
+//実行オブジェクト
+object Trait {
+	def main(args: Array[String]): Unit = {
+	   //class1
+	   println("---- traitAを実装したクラス ------");
+	   var cls1:Class1 = new Class1()
+	   cls1.f1
+	   
+	   //class2
+	   println("---- traitAとBを実装したクラス ------");
+	   var cls2:Class2 = new Class2()
+	   cls2.f1; cls2.f2
+	}
+}
+
+//trait A を実装したクラス
+class Class1 extends A{
+	def f1()={
+		println("f1")
+	}
+}
+
+//trait AとBを実装したクラス
+class Class2 extends A with B{
+   def f1() ={
+     println("f1");
+   }
+}
+//トレイトA
+trait A{
+	def f1() //メソッド宣言のみ  
+}
+//トレイトB
+trait B{
+	def f2():Unit = { //メソッド実装
+			println("f2") 
+	}
+}
