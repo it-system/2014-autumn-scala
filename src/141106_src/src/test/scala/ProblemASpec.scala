163476a949a7803ccf95efda92664844521667aa
saxsir <saxsir.256@gmail.com>
2014-11-01 00:10:20 +0900

    enumPriceCombinationのテストを書いた

--- src/141106_src/src/test/scala/ProblemASpec.scala
+++ src/141106_src/src/test/scala/ProblemASpec.scala
@@ -11,6 +11,17 @@ class ProblemASpec extends FunSpec {
       }
     }
 
+    describe("#enumPriceCombination") {
+      it("should return List[(Int, Int)]") {
+        val response = ProblemA.enumPriceCombination(105)
+        assert(response.isInstanceOf[List[(Int, Int)]])
+      }
+
+      it("should return all combination") {
+        // ちょっとこのテストは書くの大変なので自分を信じる
+      }
+    }
+
     describe("#calcMaxPrice") {
       it("should return the max value of total price(tax included)") {
         assert(ProblemA.calcMaxPrice(20, 80, 105) == 107)
