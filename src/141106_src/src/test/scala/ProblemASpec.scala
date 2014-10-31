fbd14861addeafd0bcff805a094916bd2cd7146a
saxsir <saxsir.256@gmail.com>
2014-10-31 23:03:09 +0900

    calaMaxPriceのテストを書いた

--- src/141106_src/src/test/scala/ProblemASpec.scala
+++ src/141106_src/src/test/scala/ProblemASpec.scala
@@ -2,11 +2,18 @@ import org.scalatest.FunSpec
 
 class ProblemASpec extends FunSpec {
   describe("ProblemA") {
+
     describe("#readTextFileIntoList") {
       it("should return List[String]") {
         val filePath = "src/main/resources/problem.txt"
         val response = ProblemA.readTextFileIntoList(filePath)
         assert(response.isInstanceOf[List[String]])
+      }
+    }
+
+    describe("#calcMaxPrice") {
+      it("should return the max value of total price(tax included)") {
+        assert(ProblemA.calcMaxPrice(5, 8, 105) == 109)
       }
     }
   }
