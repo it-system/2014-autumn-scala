47c6238773d010ce49de04b819b301fbd137f659
saxsir <saxsir.256@gmail.com>
2014-10-31 18:48:36 +0900

    テキストファイル読み込みのテストを書いた

--- src/141106_src/src/test/scala/ProblemASpec.scala
+++ src/141106_src/src/test/scala/ProblemASpec.scala
@@ -3,9 +3,9 @@ import org.scalatest.FunSpec
 class ProblemASpec extends FunSpec {
   describe("ProblemA") {
     describe("#readTextFileIntoList") {
-      val filePath = ""
-      it("should return list") {
-        val response = ProblemA.readTextFileIntoList()
+      it("should return List[String]") {
+        val filePath = "src/main/resources/problem.txt"
+        val response = ProblemA.readTextFileIntoList(filePath)
         assert(response.isInstanceOf[List[String]])
       }
     }
