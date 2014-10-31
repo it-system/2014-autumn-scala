245687ac096391668527712e2149db618dbb2b72
saxsir <saxsir.256@gmail.com>
2014-11-01 00:13:05 +0900

    calcMaxPriceに処理の流れのコメントを書いて、enmuPriceCombination関数を新たに追加した

--- src/141106_src/src/main/scala/ProblemA.scala
+++ src/141106_src/src/main/scala/ProblemA.scala
@@ -32,6 +32,29 @@ object ProblemA {
    * @return 税率変更後の税込合計価格の最大値
    */
   def calcMaxPrice(oldTaxRate: Int, newTaxRate: Int, oldTotalPrice: Int): Int = {
-    return 109
+    /**
+     * 税込み価格の決定ルール
+     *   1. 消費税率が x% のとき, 税抜価格が p 円である商品の税込価格は, p (100+x) / 100   円を小数点以下切り捨てたものである
+     *   2. 複数の商品についてまとめて支払う際の税込合計価格は, 個々の商品の税込価格の合計額とする
+     */
+
+    // 税込み合計価格がoldTotalPriceになる価格の組み合わせを全通り求める
+
+    // 全組み合わせに対して, 新税率での税込合計価格がいくらになるか求める
+
+    // 最大値を返す
+
+    return 0
+  }
+
+  /**
+   * 税込み合計価格が引数になるような、税抜き価格の組み合わせを求める関数
+   * @param oldTotalPrice 税率変更前の税込み合計価格
+   * @return 税抜き価格の組み合わせの配列
+   */
+  def enumPriceCombination(oldTotalPrice: Int): List[(Int, Int)] = {
+    // まず合計がoldTotalPriceになるような整数の組み合わせを列挙する
+    // 次に、その組み合わせが実現できる税抜き価格の組み合わせを列挙する
+    return List((1, 2), (3, 4))
   }
 }
