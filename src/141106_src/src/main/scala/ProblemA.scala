74627a4b6a819fb3cc2e8b65955338b53d86ca43
saxsir <saxsir.256@gmail.com>
2014-10-31 18:54:43 +0900

    ファイルの読み込みできた

--- src/141106_src/src/main/scala/ProblemA.scala
+++ src/141106_src/src/main/scala/ProblemA.scala
@@ -1,5 +1,26 @@
+/**
+ * ACM2014国内予選のProblem Aを解くプログラム
+ *
+ * cf. http://icpc.iisf.or.jp/past-icpc/domestic2014/#section_A
+ */
+
+import scala.io._
+
 object ProblemA {
   def main(args: Array[String]): Unit = {
-    println("run ProblemA#main")
+    val inputs = readTextFileIntoList("src/main/resources/problem.txt")
+    for (line <- inputs) {
+      println(line)
+    }
+  }
+
+  /**
+   * ファイルを読み込む関数
+   * @param filePath 入力例のサンプル
+   * @return テキストファイルを1行ずつ格納した配列
+   */
+  def readTextFileIntoList(filePath: String): List[String] = {
+    val list = Source.fromFile(filePath, "UTF-8").getLines.toList
+    return list
   }
 }
