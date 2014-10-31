e0893a52162bf6d2a3555e7077a80e40704b5016
saxsir <saxsir.256@gmail.com>
2014-10-31 16:56:44 +0900

    入力例を記述したテキストファイルを配置

--- src/141106_src/src/test/scala/ProblemASpec.scala
+++ src/141106_src/src/test/scala/ProblemASpec.scala
@@ -2,5 +2,12 @@ import org.scalatest.FunSpec
 
 class ProblemASpec extends FunSpec {
   describe("ProblemA") {
+    describe("#readTextFileIntoList") {
+      val filePath = ""
+      it("should return list") {
+        val response = ProblemA.readTextFileIntoList()
+        assert(response.isInstanceOf[List[String]])
+      }
+    }
   }
 }
