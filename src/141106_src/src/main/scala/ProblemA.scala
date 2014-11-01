f6595bb760835eb45f770cdcd56651990da2eff7
saxsir <saxsir.256@gmail.com>
2014-11-01 12:45:33 +0900

    合計金額と税率を与えると、税抜き前の2つの商品の金額の組み合わせを返す関数を書いた
    * returnを書かないように変更した（予期しない動作があるため）

--- src/141106_src/src/main/scala/ProblemA.scala
+++ src/141106_src/src/main/scala/ProblemA.scala
@@ -5,13 +5,16 @@
  */
 
 import scala.io._
+import math._
 
 object ProblemA {
   def main(args: Array[String]): Unit = {
-    val inputs = readTextFileIntoList("src/main/resources/problem.txt")
-    for (line <- inputs) {
-      println(line)
-    }
+    // val inputs = readTextFileIntoList("src/main/resources/problem.txt")
+    // for (line <- inputs) {
+    //   println(line)
+    // }
+
+    calcMaxPrice(5, 8, 105)
   }
 
   /**
@@ -20,8 +23,7 @@ object ProblemA {
    * @return テキストファイルを1行ずつ格納した配列
    */
   def readTextFileIntoList(filePath: String): List[String] = {
-    val list = Source.fromFile(filePath, "UTF-8").getLines.toList
-    return list
+    Source.fromFile(filePath, "UTF-8").getLines.toList
   }
 
   /**
@@ -39,6 +41,8 @@ object ProblemA {
      */
 
     // 税込み合計価格がoldTotalPriceになる価格の組み合わせを全通り求める
+    val priceCombinationWithoutTax = enumPriceCombinationWithoutTax(oldTotalPrice, oldTaxRate)
+    println(priceCombinationWithoutTax)
 
     // 全組み合わせに対して, 新税率での税込合計価格がいくらになるか求める
 
@@ -52,19 +56,42 @@ object ProblemA {
    * @param taxIncludedTotalPrice 税込み合計価格
    * @return 税抜き価格の組み合わせの配列
    */
-  def enumPriceCombinationWithoutTax(taxIncludedTotalPrice: Int): List[List[Int]] = {
+  def enumPriceCombinationWithoutTax(taxIncludedTotalPrice: Int, oldTaxRate: Int): List[List[Int]] = {
+    //TOOD: 別のメソッドに切り分ける
     val combTaxIncluded = {(n: Int) => {
       var combination = List.empty[List[Int]]
       (1 to (n-1)/2).foreach({ i =>
         // 逆順になるが、Listはデータを先頭に追加した方が処理が早い
         combination = List(i, n-i) :: combination
       })
-      return combination
+      combination
     }: List[List[Int]] }.apply(taxIncludedTotalPrice)
 
-    // 次に、その組み合わせが実現できる税抜き価格の組み合わせを列挙する
-    // val combTaxExcluded = {}.apply()
-    //var tmp = List.empty[Int]
-    return combTaxIncluded
+    //TOOD: 別のメソッドに切り分ける
+    val combPriceWithoutTax = {(list: List[List[Int]], taxRate: Int) => {
+      // この計算は別のメソッドでも使うかもしれない、使う場合は外に出す
+      def calcTaxIncludedPrice(p: Int): Int = {
+        p * (100 + taxRate) / 100
+      }
+
+      def calcPriceWithoutTax(p: Int): Int = {
+        ceil(p * 100.0 / (100 + taxRate)).toInt
+      }
+
+      var combination = List.empty[List[Int]]
+      list.foreach({ c =>
+        //TODO: cのlengthが2以外だった場合のエラー処理を入れる
+        val item1 = calcPriceWithoutTax(c(0))
+        val item2 = calcPriceWithoutTax(c(1))
+
+        if (calcTaxIncludedPrice(item1) == c(0) && calcTaxIncludedPrice(item2) == c(1)) {
+          combination = List(item1, item2) :: combination
+        }
+      })
+      // ここでreturnを書くと即時関数の中じゃなくて enumPriceCombinationWithoutTax のreturnとして解釈される(要調査)
+      combination
+    }: List[List[Int]] }.apply(combTaxIncluded, oldTaxRate)
+
+    combPriceWithoutTax
   }
 }
