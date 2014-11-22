d4e7f54a3de247218a808c3eee3149ab778a9d7e
saxsir <saxsir.256@gmail.com>
2014-11-22 16:15:44 +0900

    ProblemBを解く準備

---
+++ src/141127_src/src/main/scala/ProblemB.scala
@@ -0,0 +1,15 @@
+/**
+ * ACM2014国内予選のProblem Bを解くプログラム
+ *
+ * cf. http://icpc.iisf.or.jp/past-icpc/domestic2014/#section_B
+ */
+
+import scala.io._
+import math._
+import scala.collection.mutable._
+
+object ProblemB {
+  def main(args: Array[String]): Unit = {
+    println("hello, problem b")
+  }
+}
---
+++ src/141127_src/src/test/scala/ProblemrBSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSpec
+
+class ProblemBSpec extends FunSpec {
+  describe("ProblemB") {
+    describe("#ちゃんと動く") {
+    }
+  }
+}
