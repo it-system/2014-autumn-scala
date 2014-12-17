3287d3f6d0484d621ff5b8e5da044524f2d82d7b
saxsir <saxsir.256@gmail.com>
2014-12-17 21:07:50 +0900

    [add] 問題文を読み込んで、問題ごとのリストを返す関数を追加
    refs #3 問題ごとに扱いやすいようにリストで返すようにした

--- src/141127_src/src/main/scala/ProblemB.scala
+++ src/141127_src/src/main/scala/ProblemB.scala
@@ -10,6 +10,44 @@ import scala.collection.mutable._
 
 object ProblemB {
   def main(args: Array[String]): Unit = {
-    println("hello, problem b")
+    val inputs = readTextFileIntoList("src/main/resources/problem.txt")
+
+    val problems = createProblems(inputs)
+
+    for((p, i) <- problems.zipWithIndex) {
+      println(s"----- problem $i -----")
+      println(p)
+    }
+
+  }
+
+  /**
+   * @param filePath 入力例のサンプルファイルのパス
+   * @return テキストファイルを1行ずつ格納した配列
+   */
+  def readTextFileIntoList(filePath: String): List[String] = {
+    Source.fromFile(filePath, "UTF-8").getLines.toList
+  }
+
+  /**
+   * @param inputs 問題のテキストを1行ずつ格納した配列
+   * @return 問題ごとに分割された配列
+   */
+  def createProblems(inputs: List[String]): List[List[List[Int]]]= {
+    var _inputs = inputs.to[ListBuffer]
+    var problems = ListBuffer.empty[List[List[Int]]]
+
+    //REVIEW これだと最後の0も読み込んでしまうので、残り行数が1になったら終わってもいい？
+    while (_inputs.length > 0) {
+      val height = _inputs.remove(0) toInt
+      var problem = ListBuffer.empty[List[Int]]
+      for (i <- 0 until height) {
+        val line = _inputs.remove(0)
+        problem.append((line split " ").toList.map(_ toInt))
+      }
+      problems.append(problem.toList)
+    }
+
+    return problems.toList
   }
 }
