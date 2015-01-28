9cd6f7db7ce4674e59bd213b78d1f068515418fe
saxsir <saxsir.256@gmail.com>
2015-01-28 17:48:16 +0900

    [add] 問題文の繰り返し処理機能（mainの大枠）できた

--- src/150131_src/ProblemB/src/main/main.scala
+++ src/150131_src/ProblemB/src/main/main.scala
@@ -7,7 +7,21 @@ import scala.io._
 object Sample {
 
   val input = new java.util.Scanner(System.in)
+  val W = 5
 
   def main(args: Array[String]): Unit = {
+    while(true) {
+      var H = input.nextInt
+      println(H)
+
+      if (H == 0) return
+
+      for ( y <- 0 until H) {
+        for (x <- 0 until W) {
+          print(input.nextInt)
+        }
+        println()
+      }
+    }
   }
 }
