8823f75c740588f945f2b62970da0bb9c9b832ef
saxsir <saxsir.256@gmail.com>
2015-01-28 17:29:11 +0900

    [clean] Delete sample code
    サンプルソースを削除、入力を受け取るインスタンスの変数名を変更

--- src/150131_src/ProblemB/src/main/main.scala
+++ src/150131_src/ProblemB/src/main/main.scala
@@ -6,14 +6,8 @@ import scala.io._
 
 object Sample {
 
-  val sc = new java.util.Scanner(System.in)
+  val input = new java.util.Scanner(System.in)
 
   def main(args: Array[String]): Unit = {
-
-    val a,b,c = sc.next
-    println(a)
-    println(b)
-    println(c)
-
- }
+  }
 }
